6afb839c876262b4f552ec4e76584fc118cf1f70
daanoutros <anderson951727@gmail.com>
2026-08-13 20:18:28 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,18 @@
+/*
+=================================================
+                   LABORATÓRIO I
+=================================================
+NOME: ANDERSON PAULINO DA SILVA | RA: 10755805
+TURMA: 02P | ALGORITMOS E PROGRAMAÇÃO II
+*/
+
+#include <stdio.h>
+
+int main(){
+  printf("MACKENZIE - FCI/n");
+  printf("Algoritmos e ");
+  printf("Prog. II/n");
+
+  return 0;
+
+}
